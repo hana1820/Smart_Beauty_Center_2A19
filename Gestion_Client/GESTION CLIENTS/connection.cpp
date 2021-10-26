db5bae29a0a91154c2d2a2c95bda7d68f99400f5
BALLAMOUSSAKEITA <ballamoussa.keita@esprit.tn>
2021-10-26 10:47:01 +0100

    Commit Cruds

---
+++ Gestion_Client/GESTION CLIENTS/connection.cpp
@@ -0,0 +1,24 @@
+#include "connection.h"
+
+Connection::Connection()
+{
+
+}
+
+bool Connection::createconnection()
+{
+   bool test=false;
+db = QSqlDatabase::addDatabase("QODBC"); //initialisation de l'attribut de db qui sera utilisé dans la méthode create connection;
+db.setDatabaseName("test2");//inserer le nom de la source de données ODBC
+db.setUserName("system");//inserer nom de l'utilisateur
+db.setPassword("@Tampon2001");//inserer mot de passe de cet utilisateur
+
+if (db.open()) test=true;
+
+return  test;
+}
+
+void Connection::closeconnection()
+{
+    db.close();
+}
